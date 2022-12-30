39bbf9a1f30390de3eabc5fa51f1a17faaac7f78
Chanise Hart <niecy23@gmail.com>
2022-12-30 00:15:20 -0700

    Exercise 1 - Added Comments

--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -3,11 +3,13 @@ namespace StaticExercise
 {
 	public static class TempConverter
 	{
+		//Method to convert Fahrenheit to Celsius
 		public static double FahrenheitToCelsius(double fahrenheit)
 		{
 			return (fahrenheit - 32) / 1.8;
         }
 
+		//Method to convert Celsius to Fahrenheit
 		public static double CelsiusToFahrenheit(double celsius)
 		{
 			return (celsius * 9) / 5 + 32;
