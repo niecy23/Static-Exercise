f9f1faf839a0c73ca8c19151b04911f6653dcfca
Chanise Hart <niecy23@gmail.com>
2022-12-30 00:10:20 -0700

    Exercise 1 with Flare

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Peeps!");
+            Console.WriteLine($"Please enter your Fahrenheit temperature.");
+            var convToCelsius = double.Parse(Console.ReadLine());
+
+            convToCelsius = TempConverter.FahrenheitToCelsius(convToCelsius);
+
+            Console.WriteLine($"Temperature in Celsius: {convToCelsius}");
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine($"Now enter a new temperature in Celsius.");
+            var convToFahrenheit = double.Parse(Console.ReadLine());
+
+            convToFahrenheit = TempConverter.CelsiusToFahrenheit(convToFahrenheit);
+            Console.WriteLine($"Temperature in Fahrenheit: {convToFahrenheit}"); 
+
         }
     }
 }
